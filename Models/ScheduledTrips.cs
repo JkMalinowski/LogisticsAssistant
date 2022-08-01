3acdd42338ad85e275b810b33dc51194dc8c20f3
MumiaKK1337 <mumiaxd1337@gmail.com>
2022-08-01 15:18:02 +0200

    t

--- Models/ScheduledTrips.cs
+++ Models/ScheduledTrips.cs
@@ -10,16 +10,22 @@ namespace LogisticsAssistant.Models
         public int ScheduledTripId { get; set; }
         [ForeignKey("Lorries"), DisplayName("Lorry brand")]
         public int LorryId { get; set; }
+        
         [MaxLength(2000), DisplayName("Trip description")]
         public string? TripDescription { get; set; }
+        
         [DisplayName("Distance in km"), MinValue(1)]
         public int Distance { get; set; }
-        [DataType(DataType.DateTime), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}"), DisplayName("Date of departue")]
+        
+        [DataType(DataType.DateTime), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}"), DisplayName("Date of departue"), TripDateValidator]
         public DateTime DateOfDepartue { get; set; }
+        
         [DataType(DataType.DateTime), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}"), DisplayName("Date of arrival")]
         public DateTime DateOfArrival { get; set; }
+        
         [DataType(DataType.DateTime), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}"), DisplayName("Date of creation trip")]
         public DateTime CreationTripDate { get; set; }
+        
         [DisplayName("Lorry brand")]
         public virtual Lorries? Lorry { get; set; }
     }
